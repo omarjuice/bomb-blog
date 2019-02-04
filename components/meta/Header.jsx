639e259907c52c6ae34f59f4beaacb322ca81a85
Omar Juice <ojameer1@gmail.com>
2019-02-03 21:59:57 -0500

    favicon

--- components/meta/Header.jsx
+++ components/meta/Header.jsx
@@ -21,7 +21,15 @@ class Header extends Component {
                     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP"
                         crossOrigin="anonymous"></link>
                     <link href="https://fonts.googleapis.com/css?family=Baloo+Thambi|Coiny" rel="stylesheet"></link>
+
                     <title>Blog</title>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
+                    <link rel="manifest" href="/static/favicon/site.webmanifest" />
+                    <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+                    <meta name="msapplication-TileColor" content="#da532c" />
+                    <meta name="theme-color" content="#ffffff"></meta>
                 </Head>
                 <GlobalStyles />
                 <nav className="navbar" role="navigation" aria-label="main navigation">
